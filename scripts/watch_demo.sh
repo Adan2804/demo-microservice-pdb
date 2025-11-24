38fab0bc0267c87c64c08246e5014798a4234df0
Adan Mark Erick Moreto Enrriquez <amoreto@bancolombia.com.co>
2025-11-24 16:03:48 -0500

    feat: add Kubernetes service definition and a script to monitor its output

--- scripts/watch_demo.sh
+++ scripts/watch_demo.sh
@@ -5,7 +5,16 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-SERVICE_URL=$(minikube service demo-pdb-service --url)
+echo -e "${GREEN}Getting Service URL...${NC}"
+# This command gets the URL for the service (works for NodePort/LoadBalancer/ClusterIP with tunnel)
+SERVICE_URL=$(minikube service demo-pdb-service --url | head -n 1)
+
+if [ -z "$SERVICE_URL" ]; then
+  echo -e "${RED}Could not get service URL. Is the service deployed?${NC}"
+  echo "Try running: minikube service demo-pdb-service --url"
+  exit 1
+fi
+
 echo -e "${GREEN}Monitoring Service at $SERVICE_URL${NC}"
 
 while true; do
